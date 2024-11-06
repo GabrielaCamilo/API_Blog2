b820b7565e75bd9e465b7a1b71b2c68e8768732a
GabrielaCamilo <gabrielacamilovargasss@gmail.com>
2024-11-06 12:59:42 -0300

    first commit

---
+++ src/auth/constants.ts
@@ -0,0 +1,5 @@
+
+export const jwtConstants = {
+    secret: 'Q1O39tDIR2ZNFMpNkQ2ohvBHF13U6GBLQwwhVaWknaLFelwPCU',
+  };
+  
---
+++ src/users/users.service.ts
@@ -0,0 +1,25 @@
+
+import { Injectable } from '@nestjs/common';
+
+// This should be a real class/interface representing a user entity
+export type User = any;
+
+@Injectable()
+export class UsersService {
+  private readonly users = [
+    {
+      userId: 1,
+      username: 'john',
+      email: 'teste@teste.com',
+    },
+    {
+      userId: 2,
+      username: 'maria',
+      email: 'guess',
+    },
+  ];
+
+  async findOne(username: string): Promise<User | undefined> {
+    return this.users.find(user => user.username === username);
+  }
+}
